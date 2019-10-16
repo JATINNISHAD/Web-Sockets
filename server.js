e8238b63cf975b71666369ae5567c086cf50437e
Jatin Nishad <jatinwale@gmail.com>
2019-10-16 19:16:58 +0530

    update 1

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+
+const server = app.listen(5000,()=>{
+    console.log('the sever is loaded');
+})
+const io = socket(server);
+io.on('connection',(socket)=>{
+    console.log('made socket connection',socket.id);
+    socket.on('chat',(data)=>{
+        io.sockets.emit('chat',data);
+    })
+    socket.on('typing',(data)=>{
+        socket.broadcast.emit('typing',data)
+    })
+})
